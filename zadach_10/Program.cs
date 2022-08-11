21f02902f235c14005533eaae5cde51063abee5e
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2022-08-11 18:05:37 +0300

    Показывает вторую цифру трех значного числа

---
+++ zadach_10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите трех значное число ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(Number);
+Console.WriteLine("Вторая цифра ->"+stringNumber[1]);
+
