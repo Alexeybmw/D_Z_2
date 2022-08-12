ee19607e2fd593afe00ed593c90894032bbdfa5e
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2022-08-12 10:41:03 +0300

    Показывает третье число

---
+++ zadacha_13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число:");
+int Num = Convert.ToInt32(Console.ReadLine());
+string Number = Convert.ToString(Num);
+{
+    if (Number.Length > 2 )
+    {
+        Console.WriteLine("Третья цифра ->"+ Number [2] );
+    }
+    else
+
+    {
+        Console.WriteLine("-> Третьей цифры нет ");
+    }
+}
