9f2452a8c2b8ce57f13d1e07b14e74d63e6e5edc
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2022-08-12 11:31:32 +0300

    показывает день недели выходной или нет

---
+++ zadacha_15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите день недели:");
+int day = Convert.ToInt32(Console.ReadLine());
+
+void week (int day)
+{
+    if(day == 6 || day == 7)
+    {
+        Console.WriteLine("(Выходной) -> Да");
+    }
+       
+    else  Console.WriteLine("(Выходной) -> Нет "); 
+
+}
+week(day);
